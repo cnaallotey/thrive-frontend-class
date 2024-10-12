9703d63a5585cb3e235512dfbf37ca3dcb2120d8
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2024-10-12 18:43:20 +0000

    introduction to javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+const name = 'Charles is a boy';
+console.log(typeof name);
+
+let age = 12;
+console.log(typeof age);
+
+const active = false;
+console.log(typeof true);
+
+const fruits = ['banana', 12, true];
+
+
+const user = {
+  name: 'Charles Allotey',
+  age: 12,
+  isPresent: true,
+  printName: function () {
+    console.log(this.name);
+  },
+};
+
+console.log(++age)
+console.log(age)
+
+console.log (12 === 12 ? "number is 12" : "it is not a number")
